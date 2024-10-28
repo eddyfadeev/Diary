b87cebfe4de81cdeca7e3b24bac112b0cf69630d
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-10-28 01:15:32 -0600

    feat(database): Add seed data for DiaryEntries in OnModelCreating method

--- DiaryApp/Data/ApplicationDbContext.cs
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -11,4 +11,33 @@ public class ApplicationDbContext : DbContext
     }
     
     public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<DiaryEntry>().HasData(
+            new DiaryEntry
+            {
+                Id = 1,
+                Title = "Went Hiking",
+                Content = "Went hiking to Whistlers Mountain",
+                Created = DateTime.Now
+            },
+            new DiaryEntry
+            {
+                Id = 2,
+                Title = "Went Shopping",
+                Content = "Went shopping to Hinton",
+                Created = DateTime.Now
+            },
+            new DiaryEntry
+            {
+                Id = 3,
+                Title = "Went Driving",
+                Content = "Went driving to Jasper Driver's school",
+                Created = DateTime.Now
+            }
+            );
+    }
 }
