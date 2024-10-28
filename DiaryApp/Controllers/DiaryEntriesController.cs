736257befad2083fa75e049ed8a3e1702bfb68d1
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-10-28 00:55:35 -0600

    feat: Add navigation link to Diary Entries page in _Layout.cshtml
    Add a new navigation link in the shared _Layout.cshtml file to direct users to the Diary Entries page. Also, create a new DiaryEntriesController.cs file and Index.cshtml file for the DiaryEntries functionality.

---
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiaryApp.Controllers;
+
+public class DiaryEntriesController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
