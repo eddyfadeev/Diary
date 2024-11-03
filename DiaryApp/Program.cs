5b2c8ea21688af6cf94097d0b7dba1a8a91954cc
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-03 14:48:29 -0700

    feat(Startup): Add Startup class to configure services and app
    - Added Startup class with ConfigureServices and ConfigureApp methods
    - ConfigureServices method configures services for controllers and database
    - ConfigureApp method sets up app configuration for production environment
    - Reorganized Program.cs to use Startup class methods for configuration

--- DiaryApp/Program.cs
+++ DiaryApp/Program.cs
@@ -1,37 +1,11 @@
-using DiaryApp.Data;
-using Microsoft.EntityFrameworkCore;
+using DiaryApp;
 
-var builder = WebApplication.CreateBuilder(args);
+CreateHostBuilder(args).Build().Run();
+return;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(
-    options => options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-        ));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseStaticFiles();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-
-app.Run();
+static IHostBuilder CreateHostBuilder(string[] args) => 
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
---
+++ DiaryApp/Startup.cs
@@ -0,0 +1,50 @@
+﻿using DiaryApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiaryApp;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration) => 
+        _configuration = configuration;
+
+    private readonly IConfiguration _configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+        
+        services.AddDbContext<ApplicationDbContext>(
+            options => options.UseSqlServer(
+                _configuration.GetConnectionString("DefaultConnection")
+            ));
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+    {
+        if (environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseStaticFiles();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+            endpoints.MapFallbackToController("Error", "Home");
+        });
+    }
+}
