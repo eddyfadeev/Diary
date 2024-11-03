55345df9584073d3122ff9ac74aecf1f88917997
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-03 12:43:38 -0700

    feat(models): add validation attributes to DiaryEntry.cs
    Added validation attributes to the properties of the DiaryEntry class in
    DiaryEntry.cs file. Included Required, StringLength, and error messages for
    Title, Content, and Created properties to ensure data integrity and proper
    input validation.

--- DiaryApp/Models/DiaryEntry.cs
+++ DiaryApp/Models/DiaryEntry.cs
@@ -5,8 +5,16 @@ namespace DiaryApp.Models;
 public class DiaryEntry
 {
     public int Id { get; set; }
-    [Required] public string Title { get; set; } = string.Empty;
-    [Required] public string Content { get; set; } = string.Empty;
-    [Required] public DateTime Created { get; set; } = DateTime.Now;
+    
+    [Required(ErrorMessage = "Please enter a title for the entry")] 
+    [StringLength(100, MinimumLength = 3, 
+        ErrorMessage = "Title must be between 3 and 100 characters")]
+    public string Title { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Please enter how was your day")]
+    public string Content { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Please, specify the date of the entry")] 
+    public DateTime Created { get; set; } = DateTime.Now;
 }
 
