838ade76e411a66f4eca2c782542623ffd035ebf
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-03 14:25:53 -0700

    feat(diary): Add edit functionality to DiaryEntries
    Added a new file 'Edit.cshtml' for editing diary entries. Added edit button in 'Index.cshtml' to redirect to edit page. Added edit action methods in 'DiaryEntriesController.cs'.

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -32,7 +32,60 @@ public class DiaryEntriesController : Controller
             return View(entry);
         }
         
-        await _context.AddAsync(entry);
+        await _context.DiaryEntries.AddAsync(entry);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id is null or 0)
+        {
+            return NotFound();
+        }
+        
+        var entry = await _context.DiaryEntries.FindAsync(id);
+
+        if (entry is null)
+        {
+            return NotFound();
+        }
+        
+        return View(entry);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(DiaryEntry entry)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(entry);
+        }
+
+        await Task.Run(() => _context.DiaryEntries.Update(entry));
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("Index");
+    }
+
+    //[HttpPost]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id is null or 0)
+        {
+            return NotFound();
+        }
+
+        var entity = await _context.DiaryEntries.FindAsync(id);
+
+        if (entity is null)
+        {
+            return NotFound();
+        }
+
+        await Task.Run(() => _context.Remove(entity));
         await _context.SaveChangesAsync();
 
         return RedirectToAction("Index");
