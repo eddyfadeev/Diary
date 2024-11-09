e65b33c72c0da3c188daf2319dfe4eeece30119c
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-09 01:45:17 -0700

    feat(diary): Add PagingButton model and views (master)
    - Added PagingButton model with properties Name, SortingOrder, PageIndex,
      AppliedFilter, and IsActive.
    - Created _PagingButton.cshtml partial view to display a button with
      properties from the PagingButton model.
    - Updated Index.cshtml in Home directory to include _PagingButton partial
      views for Previous and Next buttons.
    - Updated _TableHeader.cshtml in Shared directory to use sortBy instead of
      sort

---
+++ DiaryApp/Models/PagingButton.cs
@@ -0,0 +1,12 @@
+﻿using DiaryApp.Enums;
+
+namespace DiaryApp.Models;
+
+public record PagingButton
+{
+    public string Name { get; init; }
+    public SortingOrder SortingOrder { get; init; }
+    public int PageIndex { get; init; }
+    public string AppliedFilter { get; init; }
+    public bool IsActive { get; init; }
+}
